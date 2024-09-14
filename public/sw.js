8c0cfea8930d0fbe6b1e0f54868f79846990eae1
Alan Vasconcelos <alandbh@gmail.com>
2024-09-14 19:51:30 -0300

    uploading custom SW

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", (event) => {
+    console.log("👷", "install", event);
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("👷", "activate", event);
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    console.log("👷", "fetch", event);
+    const url = new URL(event.request.url);
+    // If this is an incoming POST request for the
+    // registered "action" URL, respond to it.
+    if (event.request.method === "POST" && url.pathname === "/confirm") {
+        event.respondWith(
+            (async () => {
+                const formData = await event.request.formData();
+                const file = formData.get("file");
+                const filename = file.name.replaceAll(" ", "_");
+
+                console.log({ filename });
+                // const link = formData.get("link") || "";
+                const responseUrl = "/confirm?filename=" + filename;
+                return Response.redirect(responseUrl, 303);
+            })()
+        );
+    }
+});
